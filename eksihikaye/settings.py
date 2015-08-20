2bbd4b978224264917a162aab3f663882193f612
SeymaSARIGIL <seymasargil@gmail.com>
2015-08-20 19:26:11 +0300

    models completed

--- eksihikaye/settings.py
+++ eksihikaye/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stories',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -67,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'eksihikaye.wsgi.application'
 
